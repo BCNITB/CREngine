cb92fd30359c9588d233c34f3b0d76a77757b73f
BCNITB <97254492+BCNITB@users.noreply.github.com>
2023-01-14 14:00:05 +0100

    Bug about undo redo action with Empty Game Entity fixed

--- CREditor/Editors/WorldEditor/GameEntityView.xaml.cs
+++ CREditor/Editors/WorldEditor/GameEntityView.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CREditor.Components;
+using CREditor.GameProject;
+using CREditor.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,12 +22,50 @@ namespace CREditor.Editors
     /// </summary>
     public partial class GameEntityView : UserControl
     {
+        private Action _undoAction;
+        private string _propertyName;
         public static GameEntityView Instance { get; private set; }
         public GameEntityView()
         {
             InitializeComponent();
             DataContext = null;
             Instance = this;
+            DataContextChanged += (_, _) =>
+            {
+                if (DataContext != null)
+                {
+                    (DataContext as MSEntity).PropertyChanged += (s, e) => _propertyName = e.PropertyName;
+                }
+            };
+        }
+
+        private Action GetRenameAction()
+        {
+            var vm = DataContext as MSEntity;
+            var selection = vm.SelectedEntities.Select(entity => (entity, entity.Name)).ToList();
+            return new Action(() =>
+            {
+                selection.ForEach(item => item.entity.Name = item.Name);
+                (DataContext as MSEntity).Refresh();
+            });
+        }
+
+        private void OnName_TextBox_GotKeyboardFocus(object sender, KeyboardEventArgs e)
+        {
+            _undoAction = GetRenameAction();
+        }
+
+        private void OnName_TextBox_LostKeyboardFocus(object sender, KeyboardEventArgs e)
+        {
+            if(_propertyName == nameof(MSEntity.Name) && _undoAction != null)
+            {
+                var _redoAction = GetRenameAction();
+
+                Project.UndoRedo.Add(new UndoRedoAction(_undoAction, _redoAction, "Rename Game Entity"));
+                _propertyName = null;
+            }
+
+            _undoAction = null;
         }
     }
 }
